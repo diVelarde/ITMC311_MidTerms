d02d539cc477ae8764864315b880643494d0d2f5
s-pidergab <gabraposa@gmail.com>
2025-10-07 12:20:27 +0800

    ATT-01 - model with fields

---
+++ model/attendance.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const attendanceSchema = new mongoose.Schema({
+    id: int,
+    studentID: String,
+    classID: String,
+    date: { type: Date, default: Date.now },
+    status: String,
+})
+
+export default mongoose.model("attendance.js", attendanceSchema);
